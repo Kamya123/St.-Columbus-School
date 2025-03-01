66b51bdd1b3ccdd2e8ffe807a516347f5c9a1367
Kamya <kamyakumari140@gmail.com>
2025-03-01 16:55:01 +0530

    removing unnecessary codes from adminRoutes

--- backendSchool/routes/adminRoutes.js
+++ backendSchool/routes/adminRoutes.js
@@ -24,20 +24,3 @@ router.put("/schoolinfo/:id", updateSchoolInfo);
 router.delete("/schoolinfo/:id", deleteSchoolInfo);
 
 export default router;
-
-// const router = express.Router();
-
-// // Public route to get school info (no authentication)
-// // In adminRoutes.js
-// router.get("/schoolinfo/public", (req, res) => {
-//   console.log('PUBLIC ROUTE HIT'); // Check Render logs for this
-//   getSchoolInfo(req, res);
-// });
-
-// // Protected routes for admin management:
-// router.get("/schoolinfo", authenticateAdmin, getSchoolInfo);
-// router.post("/schoolinfo", authenticateAdmin, addSchoolInfo);
-// router.put("/schoolinfo/:id", authenticateAdmin, updateSchoolInfo);
-// router.delete("/schoolinfo/:id", authenticateAdmin, deleteSchoolInfo);
-
-// export default router;
